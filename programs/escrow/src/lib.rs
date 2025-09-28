78f3c397862a72861ef0a431e2e4985523ef1119
jayant818 <jayant2@s.amity.edu>
2025-09-28 18:48:36 +0530

    implement make_offer function in escrow module

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -15,6 +15,13 @@ declare_id!("bKjQMDCtnywc7dGc3YQCx9DtgA97BS7qJpWKBq77Qdu");
 #[program]
 pub mod escrow {
     use super::*;
+
+    pub fn make_offer(context:Context<MakeOffer>,id:u64,token_a_offered_amount:u64,token_b_wanted_amount:u64)->Result<()>{
+
+        handler::make_offer(context, id, token_a_offered_amount, token_b_wanted_amount)?;
+
+        Ok(())
+    }
 }
 
 // #[cfg(test)]
